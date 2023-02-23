679c637c70d272b60db3102f351932d15ed930a7
lisBernal <lis304@gmail.com>
2023-02-22 20:26:43 -0500

    Homework 1 terminada

---
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function BinarioADecimal(num) {
+   var sum = 0;
+   for (var i=0; i<num.length; i++){
+      sum+=num[i]*2**(num.length-1-i);
+   }
+   return  sum;
+  
+ 
+}
+
+function DecimalABinario(num) {
+
+   // return num.toString(2);
+
+   let almacenador = []
+    while (num!==0){
+        let residuo = num%2
+        num = Math.floor(num/2);
+        almacenador.push(residuo);}
+
+        var numeroreversa = almacenador.reverse().join('')
+        return numeroreversa
+
+}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
