a541287497740238949c5330c7ff4e32d9dc8743
LisBernal <57968858+liset304@users.noreply.github.com>
2023-03-20 19:20:31 -0500

    homework
    Homework de Algoritmos-I

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -6,6 +6,44 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+const factores = [];
+const primos = generaPrimos(num);
+if(num==0)return[0];
+if(num==1)return [1];
+let i = primos.length-1;
+while(factores[0] != 1 ){
+if(primos.includes(num)){
+  factores.unshift(num)
+  break
+}
+if(num%primos[i]==0){
+  factores.unshift(primos[i])
+  num/= Math.trunc(primos[i])
+  continue;
+}
+i--
+}
+factores.unshift(1);
+return factores
+
+function generaPrimos(num){
+  const primos = [1,2];
+  if(num<1) return [];
+  if(num==1) return[1];
+  if(num==2)return primos;
+  let actual = 3
+while (actual <=num){
+  let esPrimo=true;
+  for ( let i=2; i < actual; i++){
+    if(actual % i !==0) continue;
+    esPrimo = false
+    break;
+  }
+  if(esPrimo)primos.push(actual);
+  actual++;
+}
+return primos
+}
 
 }
 
@@ -14,7 +52,21 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length>1){
+    let ordenado;
+    do{
+      ordenado = true;
+      for(let i = 0; i < array.length-1; i++){
+        if(array[i]>array[i+1]){
+          let aux = array[i];
+          array[i] = array[i+1];
+          array[i+1]=aux;
+          ordenado = false
+        }
+      }
+    }while (!ordenado);
+  }
+  return array;
 }
 
 
@@ -23,7 +75,18 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+ 
+  for (let i = 1; i < array.length; i++ ){
+    let aux = array[i];
+    let actual = i;
+    while (actual>0 && aux < array[actual-1]){
+        array[actual] = array[--actual];
+    }
+    array[actual]=aux; // permutar
+    
+  }
+  return array
+  
 }
 
 
@@ -32,7 +95,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+ for (let i= 0 ; i < array.length-1;i++){
+  let minIndex = i;
+  for (let j=i; j<array.length; j++){
+    if(array[j]<array[minIndex])minIndex=j
+  }
+  if(i!=minIndex){
+    let aux = array[i];
+    array[i]=array[minIndex];
+    array[minIndex] = aux;
+  }
+ }
+ return array
 }
 
 
